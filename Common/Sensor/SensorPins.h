392a8aee3bf11c99e0b883949c2ada15cd6e7459
Chris Wald <wald.j.chris@gmail.com>
2019-01-14 18:37:28 -0600

    Add Arduino radio pins for test applications

--- Common/Sensor/SensorPins.h
+++ Common/Sensor/SensorPins.h
@@ -1,8 +1,15 @@
 #ifndef __SENSOR_PINS_H__
 #define __SENSOR_PINS_H__
 
+#if defined PLATFORM_TEENSY
 #define RADIO_CE    10
 #define RADIO_CSN   9
 #define RADIO_IRQ   14
+#elif defined PLATFORM_ARDUINO
+#define RADIO_CE    8
+#define RADIO_CSN   7
+#define RADIO_IRQ   2
+#endif
+
 
 #endif // __SENSOR_PINS_H__
